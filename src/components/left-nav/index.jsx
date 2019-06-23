d7d36ffe3a21b2f8718473e6a6f8aec87e23e367
buyou <lingyi88@126.com>
2019-06-23 21:56:15 +0800

    动态生成左边菜单栏

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -3,6 +3,8 @@ import {Icon,Menu} from "antd";
 import logo from '../../assets/images/logo.png'
 import './index.less'
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import menuList from '../../config/menu-config'
 const { SubMenu,Item } = Menu;
 
 
@@ -12,6 +14,40 @@ export default class Login extends Component {
     collapsed:PropTypes.bool.isRequired
   }
 
+  createMenu = (menu) => {
+    return <Item key={menu.key}>
+      <Link to={menu.key}>
+        <Icon type={menu.icon} />
+        <span>{menu.title}</span>
+      </Link>
+    </Item>
+  };
+
+    //动态创建菜单
+  componentWillMount() {
+
+    this.menus = menuList.map((menu)=>{
+      const children = menu.children;
+      //判断是否是二级菜单
+      if(children){
+        return <SubMenu
+          key={menu.key}
+          title={
+              <span>
+                <Icon type={menu.icon} />
+                <span>{menu.title}</span>
+              </span>
+          }
+        >
+          {
+            children.map((item)=> this.createMenu(item))
+          }
+        </SubMenu>
+      }else {
+        return this.createMenu(menu)
+      }
+    })
+  }
 
   render() {
     const { collapsed } = this.props;
@@ -21,64 +57,10 @@ export default class Login extends Component {
           <img src={logo} alt=""/>
           <h1 style={{display:collapsed?'none':'block'}}>硅谷后台</h1>
         </div>
-
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Item key="1">
-            <Icon type="home" />
-            <span>首页</span>
-          </Item>
-
-          <SubMenu
-            key="sub1"
-            title={
-                <span>
-                    <Icon type="appstore" />
-                    <span>商品</span>
-                </span>
-            }
-          >
-            <Item key="3">
-              <Icon type="unordered-list" />
-              <span>品类管理</span>
-            </Item>
-            <Item key="4">
-              <Icon type="tool" />
-              <span>商品管理</span>
-            </Item>
-          </SubMenu>
-
-          <Item key="5">
-            <Icon type="user" />
-            <span>用户管理</span>
-          </Item>
-
-          <Item key="6">
-            <Icon type="safety-certificate" />
-            <span>权限管理</span>
-          </Item>
-
-          <SubMenu
-            key="sub2"
-            title={
-                    <span>
-                     <Icon type="area-chart" />
-                     <span>图形图表</span>
-                    </span>
-            }
-          >
-            <Item key="7">
-              <Icon type="bar-chart" />
-              <span>柱形图</span>
-            </Item>
-            <Item key="8">
-              <Icon type="line-chart" />
-              <span>折线图</span>
-            </Item>
-            <Item key="9">
-              <Icon type="pie-chart" />
-              <span>饼图</span>
-            </Item>
-          </SubMenu>
+          {
+            this.menus
+          }
         </Menu>
       </div>
     )
